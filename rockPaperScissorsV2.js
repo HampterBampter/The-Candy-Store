b4442361f0d8cea7df7b0ad72ecfa9d7b59131ae
Senna Smith <sennaasmith@gmail.com>
2023-04-07 15:51:56 -0400

    Create rockPaperScissorsV2.js

---
+++ rockPaperScissorsV2.js
@@ -0,0 +1,72 @@
+const hands = ['rock', 'paper', 'scissors'];
+function getHand() {
+    return hands[parseInt(Math.random() * 10) % 3]
+};
+
+let playerOne = {
+    name: 'Player 1',
+    getHand: getHand(),
+    wins: 0,
+};
+
+
+let playerTwo = {
+    name: 'Player 2',
+    getHand: getHand(),
+    wins: 0,
+};
+
+function playRound(playerOne, playerTwo) {
+    playerOne.getHand = getHand();
+    playerTwo.getHand = getHand();
+
+    if (playerOne.getHand == playerTwo.getHand) {
+        console.log(`${playerOne.name}:${playerOne.wins} played ${playerOne.getHand}`)
+        console.log(`${playerTwo.name}:${playerTwo.wins} played ${playerTwo.getHand}`)
+        console.log("It's a tie!\n")
+        return null
+    }
+    else if ((playerOne.getHand == 'rock' && playerTwo.getHand == 'scissors') || (playerOne.getHand == 'paper' && playerTwo.getHand == 'rock') || (playerOne.getHand == 'scissors' && playerTwo.getHand == 'paper')) {
+        playerOne.wins++
+        console.log(`${playerOne.name}:${playerOne.wins} played ${playerOne.getHand}`)
+        console.log(`${playerTwo.name}:${playerTwo.wins} played ${playerTwo.getHand}`)
+        console.log(`${playerOne.name} wins!\n`)
+        return playerOne
+    }
+    else {
+        playerTwo.wins++
+        console.log(`${playerOne.name}:${playerOne.wins} played ${playerOne.getHand}`)
+        console.log(`${playerTwo.name}:${playerTwo.wins} played ${playerTwo.getHand}`)
+        console.log(`${playerTwo.name} wins!\n`)
+        return playerTwo
+    }
+}
+playerTwo.name = 'Hampter Bampter';
+playerOne.name = 'Glam Sally';
+let numberOfWins = 3 
+
+// while (playerOne.wins < numberOfWins && playerTwo.wins < numberOfWins) {
+//     playRound(playerOne, playerTwo);
+// }
+    function playGame(){
+       let playUntil = 3
+       let player1 = playerOne;
+       let player2 = playerTwo;
+            if player1.wins < playUntil && player2.wins < playUntil{
+            return playGame()
+            }
+            else{
+                return null
+            }
+}
+ playTournament()
+ playerOne
+ playerTwo
+ let playerThree = playerOne
+ let playerFour
+
+
+
+
+
+

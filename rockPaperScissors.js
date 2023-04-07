01eb899be70964937c4a42472115d7b51cd0a956
Senna Smith <sennaasmith@gmail.com>
2023-04-06 21:30:00 -0400

    Update rockPaperScissors.js

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -4,14 +4,14 @@ function getHand() {
 };
 
 let playerOne = {
-    name: '',
+    name: 'Player 1',
     getHand: getHand(),
     wins: 0,
 };
 
 
 let playerTwo = {
-    name: '',
+    name: 'Player 2',
     getHand: getHand(),
     wins: 0,
 };
@@ -23,18 +23,28 @@ function playRound(playerOne, playerTwo) {
     if (playerOne.getHand == playerTwo.getHand) {
         console.log(`${playerOne.name}:${playerOne.wins} played ${playerOne.getHand}`)
         console.log(`${playerTwo.name}:${playerTwo.wins} played ${playerTwo.getHand}`)
-        console.log("It's a tie!")
+        console.log("It's a tie!\n")
         return null
     }
-    if ((playerOne.getHand == 'rock' && playerTwo.getHand == 'scissors')||(playerOne.getHand == 'paper' && playerTwo.getHand == 'rock')||(playerOne.getHand == 'scissors' && playerTwo.getHand == 'paper')){
+    else if ((playerOne.getHand == 'rock' && playerTwo.getHand == 'scissors') || (playerOne.getHand == 'paper' && playerTwo.getHand == 'rock') || (playerOne.getHand == 'scissors' && playerTwo.getHand == 'paper')) {
+        playerOne.wins++
         console.log(`${playerOne.name}:${playerOne.wins} played ${playerOne.getHand}`)
         console.log(`${playerTwo.name}:${playerTwo.wins} played ${playerTwo.getHand}`)
-        console.log(`${playerOne.name} wins!`)
-        playerOne.wins++
+        console.log(`${playerOne.name} wins!\n`)
         return playerOne
     }
+    else {
+        playerTwo.wins++
+        console.log(`${playerOne.name}:${playerOne.wins} played ${playerOne.getHand}`)
+        console.log(`${playerTwo.name}:${playerTwo.wins} played ${playerTwo.getHand}`)
+        console.log(`${playerTwo.name} wins!\n`)
+        return playerTwo
+    }
 }
-console.log(`${playerOne.name} replace text ${playerTwo.name} replace text`)
+playerTwo.name = 'Hampter Bampter';
+playerOne.name = 'Glam Sally';
+let numberOfWins = 3
 
-console.log(playerTwo.name = 'Hampter Bampter');
-console.log(playerOne.name = 'Glam Sally');
+while (playerOne.wins < numberOfWins && playerTwo.wins < numberOfWins) {
+    playRound(playerOne, playerTwo);
+}

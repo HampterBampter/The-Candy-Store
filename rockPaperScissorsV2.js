098a6e127115b2ce30255c33f0ba1816d0b1dfd2
Senna Smith <sennaasmith@gmail.com>
2023-04-07 16:35:44 -0400

    added playGame and playTournament

--- rockPaperScissorsV2.js
+++ rockPaperScissorsV2.js
@@ -3,67 +3,85 @@ function getHand() {
     return hands[parseInt(Math.random() * 10) % 3]
 };
 
-let playerOne = {
+let player1 = {
     name: 'Player 1',
     getHand: getHand(),
     wins: 0,
 };
 
 
-let playerTwo = {
+let player2 = {
     name: 'Player 2',
     getHand: getHand(),
     wins: 0,
 };
 
-function playRound(playerOne, playerTwo) {
-    playerOne.getHand = getHand();
-    playerTwo.getHand = getHand();
+let player3 = {
+    name: 'Player 3',
+    getHand: getHand(),
+    wins: 0,
+};
 
-    if (playerOne.getHand == playerTwo.getHand) {
-        console.log(`${playerOne.name}:${playerOne.wins} played ${playerOne.getHand}`)
-        console.log(`${playerTwo.name}:${playerTwo.wins} played ${playerTwo.getHand}`)
+let player4 = {
+    name: 'Player 4',
+    getHand: getHand(),
+    wins: 0,
+};
+
+function playRound(player1, player2) {
+    player1.getHand = getHand();
+    player2.getHand = getHand();
+
+    if (player1.getHand == player2.getHand) {
+        console.log(`${player1.name}:${player1.wins} played ${player1.getHand}`)
+        console.log(`${player2.name}:${player2.wins} played ${player2.getHand}`)
         console.log("It's a tie!\n")
         return null
     }
-    else if ((playerOne.getHand == 'rock' && playerTwo.getHand == 'scissors') || (playerOne.getHand == 'paper' && playerTwo.getHand == 'rock') || (playerOne.getHand == 'scissors' && playerTwo.getHand == 'paper')) {
-        playerOne.wins++
-        console.log(`${playerOne.name}:${playerOne.wins} played ${playerOne.getHand}`)
-        console.log(`${playerTwo.name}:${playerTwo.wins} played ${playerTwo.getHand}`)
-        console.log(`${playerOne.name} wins!\n`)
-        return playerOne
+    else if ((player1.getHand == 'rock' && player2.getHand == 'scissors') || (player1.getHand == 'paper' && player2.getHand == 'rock') || (player1.getHand == 'scissors' && player2.getHand == 'paper')) {
+        player1.wins++
+        console.log(`${player1.name}:${player1.wins} played ${player1.getHand}`)
+        console.log(`${player2.name}:${player2.wins} played ${player2.getHand}`)
+        console.log(`${player1.name} wins!\n`)
+        return player1
     }
     else {
-        playerTwo.wins++
-        console.log(`${playerOne.name}:${playerOne.wins} played ${playerOne.getHand}`)
-        console.log(`${playerTwo.name}:${playerTwo.wins} played ${playerTwo.getHand}`)
-        console.log(`${playerTwo.name} wins!\n`)
-        return playerTwo
+        player2.wins++
+        console.log(`${player1.name}:${player1.wins} played ${player1.getHand}`)
+        console.log(`${player2.name}:${player2.wins} played ${player2.getHand}`)
+        console.log(`${player2.name} wins!\n`)
+        return player2
     }
 }
-playerTwo.name = 'Hampter Bampter';
-playerOne.name = 'Glam Sally';
-let numberOfWins = 3 
-
-// while (playerOne.wins < numberOfWins && playerTwo.wins < numberOfWins) {
-//     playRound(playerOne, playerTwo);
+player1.name = 'Hampter Bampter';
+player2.name = 'Glam Sally';
+player3.name = 'Sweeaboo';
+player4.name = 'Monito';
+// let numberOfWins = 3 
+// let playUntil = 3
+// while (player1.wins < numberOfWins && player2.wins < numberOfWins) {
+//     playRound(player1, player2);
 // }
-    function playGame(){
-       let playUntil = 3
-       let player1 = playerOne;
-       let player2 = playerTwo;
-            if player1.wins < playUntil && player2.wins < playUntil{
-            return playGame()
-            }
-            else{
-                return null
-            }
+    function playGame(player1, player2, playUntil){
+        while (player1.wins < playUntil && player2.wins < playUntil) {
+                 playRound(player1, player2);
+        }       
+        if (player1.wins > player2.wins){
+            return player1;
+        }
+        else{
+            return player2;
+        }
 }
- playTournament()
- playerOne
- playerTwo
- let playerThree = playerOne
- let playerFour
+    
+    
+ function playTournament(player1, player2, player3, player4, playUntil){
+    let game1 = playGame(player1, player2, playUntil);
+    let game2 = playGame(player3, player4, playUntil);
+    let game3 = playGame(game1, game2, playUntil);
+    console.log(`${game3.name} is the world champion!`)
+ }
+ playTournament(player1, player2, player3, player4, 5)
 
 
 

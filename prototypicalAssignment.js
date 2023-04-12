0b8fd3fab4fc4a9fb2885bb0725d8605706f0d73
Senna Smith <sennaasmith@gmail.com>
2023-04-12 00:27:49 -0400

    Update prototypicalAssignment.js

--- prototypicalAssignment.js
+++ prototypicalAssignment.js
@@ -1,21 +1,38 @@
-class Person{
-    constructor(firstName, lastName, favoriteColor, favoriteNumber, favoriteFoods){
+class Person {
+    constructor(firstName, lastName, favoriteColor, favoriteNumber, favoriteFoods) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.favoriteColor = favoriteColor;
         this.favoriteNumber = favoriteNumber;
         this.favoriteFoods = favoriteFoods;
     }
-    Person.prototype.fullName(){
-        console.log(this.firstName + this.lastName);
-        return this.firstName + this.lastName;
-    }
-    Person.prototype.toString(){
-        return `Name: ${this.fullName}`
-    }
 }
+Person.prototype.fullName = function () {
+    return `${this.firstName} ${this.lastName}`;
+}
+Person.prototype.toString = function () {
+    return `${this.fullName()}, Favorite Color: ${this.favoriteColor}, Favorite Number: ${this.favoriteNumber}`
+}
+Person.prototype.family = [];
+
+Person.prototype.addToFamily = function (familyMember) {
+    if (familyMember instanceof Person) {
+        if (this.family.includes(familyMember)) {
+        }
+        else {
+            this.family.push(familyMember);
+            }
+    }
+    return this.family.length;
+}
+
+    let glamSally = new Person('Sally', 'Glam', 'Lilac', 1, ['Mealworms', 'Sunflower Seeds', "Peanuts"]);
+    let Aldo = new Person('Aldi', 'To', 'Blue', 21, ['Tacos', 'Sushi', 'Cheesecake'])
+    let Monito = new Person('Monito', 'Kong', 'Yellow', '3', ['Bananas', 'Cheese', 'Chocolate Balls'])
     
-let glamSally = new Person('Sally', 'Glam', 'Lilac', '1', ['Mealworms', 'Sunflower Seeds']);
-glamSally.fullName();
-
-
+    console.log(glamSally.toString());
+    console.log(glamSally.addToFamily(Aldo));
+    console.log(glamSally.addToFamily(Aldo));
+    console.log(glamSally.addToFamily(Monito));
+    console.log(glamSally.addToFamily('Chomi'));
+    

afedb03fbeb8976f9e3ee93d316cb729685afc2b
Senna Smith <sennaasmith@gmail.com>
2023-04-19 19:08:46 -0400

    added button to play game

--- rockPaperScissorsV3/myscripts.js
+++ rockPaperScissorsV3/myscripts.js
@@ -34,6 +34,8 @@ function playRound(player1, player2) {
 
     let scoreP1 = document.getElementById('scoreP1');
     let scoreP2 = document.getElementById('scoreP2');
+    let scoreP3 = document.getElementById('scoreP3');
+    let scoreP4 = document.getElementById('scoreP4');
     let scoreTog = scoreP1 + scoreP2 
 
     if (player1.getHand == player2.getHand) {
@@ -45,18 +47,21 @@ function playRound(player1, player2) {
     else if ((player1.getHand == 'rock' && player2.getHand == 'scissors') || (player1.getHand == 'paper' && player2.getHand == 'rock') || (player1.getHand == 'scissors' && player2.getHand == 'paper')) {
         player1.wins++
         scoreP1.innerHTML = (`Games Won:${player1.wins} played ${player1.getHand}`)
-        coreP2.innerHTML = (`Games Won:${player2.wins} played ${player2.getHand}`)
+        scoreP2.innerHTML = (`Games Won:${player2.wins} played ${player2.getHand}`)
         scoreTog.innerHTML = (`${player1.name} wins!\n`)
         return player1
     }
     else {
         player2.wins++
         scoreP1.innerHTML = (`Games Won:${player1.wins} played ${player1.getHand}`)
-        coreP2.innerHTML = (`Games Won:${player2.wins} played ${player2.getHand}`)
+        scoreP2.innerHTML = (`Games Won:${player2.wins} played ${player2.getHand}`)
         scoreTog.innerHTML = (`${player2.name} wins!\n`)
         return player2
     }
 }
+
+let scoreP1 = document.getElementById('scoreP1');
+let scoreP2 = document.getElementById('scoreP2');
 
 function playGame(player1, player2, playUntil) {
     player1.wins = 0;
@@ -65,7 +70,7 @@ function playGame(player1, player2, playUntil) {
         playRound(player1, player2);
     }
     if (player1.wins > player2.wins) {
-        return player1;        
+        return player1; 
     }
     else {
         return player2;
@@ -85,5 +90,11 @@ player1.name = 'Hampter Bampter';
 player2.name = 'Glam Sally';
 player3.name = 'Sweeaboo';
 player4.name = 'Monito';
-playTournament(player1, player2, player3, player4, 5)
+
+let buttonPlay = document.getElementById('buttonPlay');
+buttonPlay.style.height = '35px';
+buttonPlay.style.width = '125px';
+buttonPlay.addEventListener('click', playTournament(player1, player2, player3, player4, 5));
+
+// playTournament(player1, player2, player3, player4, 5)
 

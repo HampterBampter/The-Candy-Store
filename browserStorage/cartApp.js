dcb441bb26286c54a212f14f51cae7c1d4a8c6e3
Senna Smith <sennaasmith@gmail.com>
2023-04-19 13:57:27 -0400

    create browserStorage

---
+++ browserStorage/cartApp.js
@@ -0,0 +1,39 @@
+initializedCart = function(){
+if (localStorage.getItem("cart") == null){
+    let cart = [];
+    let cartString = JSON.stringify(cart);
+    localStorage.setItem("cart", cartString);
+}
+}
+
+addItem = function(item){
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    cart.push(item);
+    let cartString = JSON.stringify(cart);
+    localStorage.setItem("cart", cartString);
+}
+
+removeItem = function(item){
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    cart.removeItem(item);
+    let cartString = JSON.stringify(cart);
+    localStorage.setItem("cart", cartString);
+}
+
+displayCart = function(){
+    let cart = localStorage.getItem("cart");
+    console.log(cart);
+}
+
+let submitButton = function (){
+    let itemName = document.getElementById("itemName");
+    let itemPrice = document.getElementById("itemPrice");
+    addItem(itemName.value, itemPrice.value);
+}
+let addItemForm = document.getElementById("addItemForm");
+addItemForm.addEventListener("click", submitButton());
+
+let displayCartButton = document.getElementById("displayCartButton");
+displayCartButton.addEventListener("click", displayCart());
+
+

9f1ad59799043922535912159280765305b01dbd
Senna Smith <sennaasmith@gmail.com>
2023-04-19 15:31:13 -0400

    formatting js

--- browserStorage/cartApp.js
+++ browserStorage/cartApp.js
@@ -1,37 +1,37 @@
-initializedCart = function(){
-if (localStorage.getItem("cart") == null){
-    let cart = [];
-    let cartString = JSON.stringify(cart);
-    localStorage.setItem("cart", cartString);
-}
+initializedCart = function () {
+    if (localStorage.getItem("cart") == null) {
+        let cart = [];
+        let cartString = JSON.stringify(cart);
+        localStorage.setItem("cart", cartString);
+    }
 }
 
-addItem = function(item){
+addItem = function (item) {
     let cart = JSON.parse(localStorage.getItem("cart"));
     cart.push(item);
     let cartString = JSON.stringify(cart);
     localStorage.setItem("cart", cartString);
 }
 
-removeItem = function(item){
+removeItem = function (item) {
     let cart = JSON.parse(localStorage.getItem("cart"));
     cart.removeItem(item);
     let cartString = JSON.stringify(cart);
     localStorage.setItem("cart", cartString);
 }
 
-displayCart = function(){
+displayCart = function () {
     let cart = localStorage.getItem("cart");
     console.log(cart);
 }
 
-let submitButton = function (event){
+let submitButton = function (event) {
     event.preventDefault();
     let itemName = document.getElementById("itemName").value;
     let itemPrice = document.getElementById("itemPrice").value;
-    addItems = {itemName, itemPrice};
-    addItem(addItems);
+    addItems = { itemName, itemPrice };
 }
+
 let addItemForm = document.getElementById("addItemForm");
 addItemForm.addEventListener("submit", submitButton);
 

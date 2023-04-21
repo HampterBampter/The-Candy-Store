9291469f53320fea91481ff5eeaf8cbcaf07314b
Senna Smith <sennaasmith@gmail.com>
2023-04-20 21:21:13 -0400

    fixed bugs and formatting. Created another function to search for a users id

--- mockingCalls.js
+++ mockingCalls.js
@@ -1,21 +1,28 @@
 let users = [
-    {id: 1, name: 'HampterBampter', email: 'sennaasmith@gmail.com'},
-    {id: 2, name: 'GlamSally', email: '2glam4u@gmail.com'},  
+    { id: 1, name: 'HampterBampter', email: 'sennaasmith@gmail.com' },
+    { id: 2, name: 'GlamSally', email: '2glam4u@gmail.com' },
 ];
 
-let fetchUserById = function(userId){
-    new promise((resolve, reject) => {
+let fetchUserById = function (userId) {
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
             let user = users.find((user) => user.id == userId);
-            if (user == null){
+            if (user == null) {
                 reject('Error: user does not exist');
             }
-            else{
+            else {
                 resolve(user);
             }
         }, 1000);
     })
 }
 
-console.log(fetchUserById(1));
+let userById = function (userId) {
+    fetchUserById(userId)
+        .then((resolved) => { console.log(resolved) })
+        .catch((error) => { console.log(error) })
+}
 
+userById(1);
+userById(2);
+userById(3);

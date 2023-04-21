a4444d7aaf8d0bc0abd6974f3ff3dc49c2d04add
Senna Smith <sennaasmith@gmail.com>
2023-04-21 11:07:12 -0400

    fixed buttons and updated some functions to replace posts with new data

--- restActivity/rest.js
+++ restActivity/rest.js
@@ -6,7 +6,7 @@ let clear = function() {
 
 let pasteResults = function(data){
     let newRes = document.createElement('p');
-    newRes.innerText = json.stringify(data);
+    newRes.innerText = JSON.stringify(data);
     results.appendChild(newRes);
 }
 
@@ -29,15 +29,16 @@ let getIdTen = function(){
 
 let newPost = function(){
     clear();
-    fetch('http://jsonplaceholder.typicode.com/posts'), {
+    fetch('http://jsonplaceholder.typicode.com/posts', {
         method: 'POST',
         headers: {
             'contentType': 'application/json; charset=UTF-8'
         },
-        body: json.stringify({
-            name: 'User 1'
+        body: JSON.stringify({
+            title: 'Day in the life of a hamster @GlamSally',
+            body: "I woke up this morning feeling hungry, and decided to go for a walk in search of FRESH food. Fresh food just tastes sooo much better than leftovers. Today was a good day, as there were worms and sunflower seeds everywhere I went. Once I was feeling satisfied, I started stuffing my cheek pouches. It was hard work carrying it all, because all that food was becoming unbelievably heavy. I was about to give up, but luckily one of my followers spotted me struggling and helped carry the load. Once I returned to my cozy den, I unloaded today's haul into my pile of food. I'm going to have to create a second room to store it all. Does anyone want to help out with that? As payment, you will receive a shoutout on my Instagram <3",
         })
-    }
+    })
 .then((res) => res.json())
 .then((data) => pasteResults(data))
 .catch((error) => console.log(`'error:' ${error}`))
@@ -45,15 +46,18 @@ let newPost = function(){
 
 let replaceTwelve = function(){
     clear();
-    fetch('http://jsonplaceholder.typicode.com/posts/12'), {
+    fetch('http://jsonplaceholder.typicode.com/posts/12', {
         method: 'PUT',
         headers: {
             'contentType': 'application/json; charset=UTF-8'
         },
-        body: json.stringify({
-            name: 'User 1'
+        body: JSON.stringify({
+            userId: 2,
+            id: 12,
+            title: 'UPDATE Day in the life of a hamster @GlamSally',
+            body: "You know that follower from last week who helped carry my food home? They came to my house today to help create a second food storage area, but they wanted some of the seeds as payment, as if an Instagram shoutout isn't enough. I can't believe it!",
         })
-    }
+    })
 .then((res) => res.json())
 .then((data) => pasteResults(data))
 .catch((error) => console.log(`'error:' ${error}`))
@@ -61,15 +65,15 @@ let replaceTwelve = function(){
 
 let updateTitle = function(){
     clear();
-    fetch('http://jsonplaceholder.typicode.com/posts'), {
+    fetch('http://jsonplaceholder.typicode.com/posts/12', {
         method: 'PATCH',
         headers: {
             'contentType': 'application/json; charset=UTF-8'
         },
-        body: json.stringify({
-            name: 'User 1'
+        body: JSON.stringify({
+            title: 'Anyone available next week? @GlamSally',
         })
-    }
+    })
 .then((res) => res.json())
 .then((data) => pasteResults(data))
 .catch((error) => console.log(`'error:' ${error}`))
@@ -80,8 +84,7 @@ let deletePost = function(){
         fetch('http://jsonplaceholder.typicode.com/posts/12'), {
             method: 'DELETE',
         }
-    .then((res) => res.json())
-    .then((data) => pasteResults(`successfully deleted ${data}`))
+    .then(() => ('successfully deleted'))
     .catch((error) => console.log(`'error:' ${error}`))
 }
 

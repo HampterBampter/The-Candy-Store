4b8db3fccb0581564a1a84c3155df2d52b45f80e
Senna Smith <sennaasmith@gmail.com>
2023-04-21 22:36:26 -0400

    Update weather.js

--- weatherAPI/weather.js
+++ weatherAPI/weather.js
@@ -2,24 +2,42 @@
 //zip: https://api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}
 // api key: 607df5c1d6479183f21c8f86b2fa6978
 
-let pasteResults = function(data){
-    let addResults = document.getElementById('addResults');
-    let enterZip = document.getElementById('enterZip').value;
-    document.getElementById('enterZip').value = '';
-    let getResults = document.createElement('p');
-    getResults.textContext.innerHTML = enterZip.JSON.strigify(data)
-    addResults.appendChild(getResults);
+
+let currentDate = document.getElementById('currentDate');
+let city = document.getElementById('city');
+let currentTemp = document.getElementById('currentTemp');
+let currentCond = document.getElementById('currentCond');
+let tempHiLow = document.getElementById('tempHiLow');
+
+
+let pasteResults = async function (data) {
+    currentDate.innerHTML = data.dt;
+    city.innerHTML = data.name;
+    currentTemp.innerHTML = data.main.temp;
+    currentCond.innerHTML = data.weather.description;
+    tempHiLow.innerHTML = `Temperature Low: ${data.main.temp_min} High: ${data.main.temp_max}`;
+
+
+
+
 }
 
-let getWeather = function(zipCode){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + zipCode + '&units=imperial&appid=607df5c1d6479183f21c8f86b2fa6978')
-    .then((res) => res.json())
-    .then((data) => pasteResults(data))
-    .catch((error) => console.log(`'error:' ${error}`))
+let getWeather = async function (zipCode) {
+    try {
+        let response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + zipCode + ',US&units=imperial&appid=607df5c1d6479183f21c8f86b2fa6978');
+        let data = await response.json();
+        pasteResults(data);
+    } catch (error) {
+        console.error("An error occurred:", error);
+    }
 }
 
-let button = document.getElementById('button').addEventListener('click', getWeather);
+let button = document.getElementById('button');
+button.addEventListener('click', () => {
+    // let zipCode = document.getElementById('zipCode').value;
+    getWeather('55444');
+});
 
-getWeather(55444);
+
 
 

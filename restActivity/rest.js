7882345e0bd4916a95d3f6f6dfe1f895ca9d7482
Senna Smith <sennaasmith@gmail.com>
2023-04-21 22:36:21 -0400

    Update rest.js

--- restActivity/rest.js
+++ restActivity/rest.js
@@ -1,33 +1,33 @@
 let results = document.getElementById('results');
 
-let clear = function() {
+let clear = function () {
     results.innerHTML = '';
 }
 
-let pasteResults = function(data){
+let pasteResults = function (data) {
     let newRes = document.createElement('p');
     newRes.innerText = JSON.stringify(data);
     results.appendChild(newRes);
 }
 
-let getAll = function(){
+let getAll = function () {
     clear();
     fetch('http://jsonplaceholder.typicode.com/posts')
-.then((res) => res.json())
-.then((data) => pasteResults(data))
-.catch((error) => console.log(`'error:' ${error}`))
+        .then((res) => res.json())
+        .then((data) => pasteResults(data))
+        .catch((error) => console.log(`'error:' ${error}`))
 }
 
-let getIdTen = function(){
+let getIdTen = function () {
     clear();
     fetch('http://jsonplaceholder.typicode.com/posts/10')
-.then((res) => res.json())
-.then((data) => pasteResults(data))
-.catch((error) => console.log(`'error:' ${error}`))
+        .then((res) => res.json())
+        .then((data) => pasteResults(data))
+        .catch((error) => console.log(`'error:' ${error}`))
 }
 
 
-let newPost = function(){
+let newPost = function () {
     clear();
     fetch('http://jsonplaceholder.typicode.com/posts', {
         method: 'POST',
@@ -39,12 +39,12 @@ let newPost = function(){
             body: "I woke up this morning feeling hungry, and decided to go for a walk in search of FRESH food. Fresh food just tastes sooo much better than leftovers. Today was a good day, as there were worms and sunflower seeds everywhere I went. Once I was feeling satisfied, I started stuffing my cheek pouches. It was hard work carrying it all, because all that food was becoming unbelievably heavy. I was about to give up, but luckily one of my followers spotted me struggling and helped carry the load. Once I returned to my cozy den, I unloaded today's haul into my pile of food. I'm going to have to create a second room to store it all. Does anyone want to help out with that? As payment, you will receive a shoutout on my Instagram <3",
         })
     })
-.then((res) => res.json())
-.then((data) => pasteResults(data))
-.catch((error) => console.log(`'error:' ${error}`))
+        .then((res) => res.json())
+        .then((data) => pasteResults(data))
+        .catch((error) => console.log(`'error:' ${error}`))
 }
 
-let replaceTwelve = function(){
+let replaceTwelve = function () {
     clear();
     fetch('http://jsonplaceholder.typicode.com/posts/12', {
         method: 'PUT',
@@ -58,12 +58,12 @@ let replaceTwelve = function(){
             body: "You know that follower from last week who helped carry my food home? They came to my house today to help create a second food storage area, but they wanted some of the seeds as payment, as if an Instagram shoutout isn't enough. I can't believe it!",
         })
     })
-.then((res) => res.json())
-.then((data) => pasteResults(data))
-.catch((error) => console.log(`'error:' ${error}`))
+        .then((res) => res.json())
+        .then((data) => pasteResults(data))
+        .catch((error) => console.log(`'error:' ${error}`))
 }
 
-let updateTitle = function(){
+let updateTitle = function () {
     clear();
     fetch('http://jsonplaceholder.typicode.com/posts/12', {
         method: 'PATCH',
@@ -74,25 +74,25 @@ let updateTitle = function(){
             title: 'Anyone available next week? @GlamSally',
         })
     })
-.then((res) => res.json())
-.then((data) => pasteResults(data))
-.catch((error) => console.log(`'error:' ${error}`))
+        .then((res) => res.json())
+        .then((data) => pasteResults(data))
+        .catch((error) => console.log(`'error:' ${error}`))
 }
 
-let deletePost = function(){
-        clear();
-        fetch('http://jsonplaceholder.typicode.com/posts/12', {
-            method: 'DELETE',
-        })
-    .then(() => ('successfully deleted'))
-    .catch((error) => console.log(`'error:' ${error}`))
+let deletePost = function () {
+    clear();
+    fetch('http://jsonplaceholder.typicode.com/posts/12', {
+        method: 'DELETE',
+    })
+        .then(() => ('successfully deleted'))
+        .catch((error) => console.log(`'error:' ${error}`))
 }
 
 
 
 let button1 = document.getElementById('button1')
     .addEventListener('click', getAll);
-let button2 = document.getElementById('button2')   
+let button2 = document.getElementById('button2')
     .addEventListener('click', getIdTen);
 let button3 = document.getElementById('button3')
     .addEventListener('click', newPost);
